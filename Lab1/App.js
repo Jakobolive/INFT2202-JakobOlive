43c67b40061cd71c338614d50774493cbd635978
JakobOlive <jakob.olive@dcmail.ca>
2024-01-29 01:04:59 -0500

    Added to the App.css file and tested out some styles and classes. Also added some starting text onto the HomePage.html from the App.js.

--- Lab1/App.js
+++ Lab1/App.js
@@ -5,3 +5,21 @@
     File: App.js
     File Desc: ...
 */
+/*
+    Section that will define strings, create elements, and use DOM manipulation to populate the HomePage.html page.  
+*/
+// Defining the strings wanted for the tags.
+let homeHeaderString = "Welcome To The Jungle";
+let homeTextString = "This is going into the homepage, will add to later.";
+// Grabbing the home page div to append to.
+var homePage = document.getElementById("homePage");
+// Creating elements and text nodes for both the header and the paragraph.
+let homeHeaderElement = document.createElement("h1");
+let homeTextElement = document.createElement("p");
+let homeHeaderNode = document.createTextNode(homeHeaderString);
+let homeTextNode = document.createTextNode(homeTextString);
+// Appending the nodes together and then onto the homePage variable, the actual page.
+homeHeaderElement.appendChild(homeHeaderNode);
+homeTextElement.appendChild(homeTextNode);
+homePage.appendChild(homeHeaderElement);
+homePage.appendChild(homeTextElement);
