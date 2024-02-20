b89714ccde3ccb31419798d94e8255b3de2a7e97
JakobOlive <jakob.olive@dcmail.ca>
2024-02-20 15:55:14 -0500

    Initial commit for ICE 5.

---
+++ ICE5/scripts/fetch_script.js
@@ -0,0 +1,16 @@
+console.log('fetch_script.js loaded')
+
+let url_posts = 'https://jsonplaceholder.typicode.com/posts';
+let url_comments = 'https://jsonplaceholder.typicode.com/comments';
+let url_albums = 'https://jsonplaceholder.typicode.com/albums';
+let url_photos = 'https://jsonplaceholder.typicode.com/photos';
+let url_todos = 'https://jsonplaceholder.typicode.com/todos';
+let url_users = 'https://jsonplaceholder.typicode.com/users';
+
+// create the fetch function
+
+// for each url variable above, call the fetch function in a console.log
+
+// from the photos url, put 2 random photos into the img elements in the html
+
+// to see the results, right click on the page and click inspect, and click the console tab
