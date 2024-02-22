9e2243eb711e1c9f43bb68cd03d263f859dfe1ab
JakobOlive <jakob.olive@dcmail.ca>
2024-02-22 14:20:54 -0500

    ICE6 is finished, final commit.

--- ICE6/scripts/ajax_script.js
+++ ICE6/scripts/ajax_script.js
@@ -1,3 +1,4 @@
+// ICE 6
 console.log('ajax_script.js loaded')
 
 let url_posts = 'https://jsonplaceholder.typicode.com/posts';
@@ -8,11 +9,23 @@ let url_todos = 'https://jsonplaceholder.typicode.com/todos';
 let url_users = 'https://jsonplaceholder.typicode.com/users';
 
 // create the function that uses jquery get
-
+function ajax_get(url){
+    $.get(url, (data) => {
+        console.log(data);
+    })
+}
 // for each url variable above, call the function in a console.log
-
+ajax_get(url_posts);
+ajax_get(url_comments);
+ajax_get(url_albums);
+ajax_get(url_todos);
+ajax_get(url_users);
 // from the photos url, put 2 random photos into the img elements in the html
-
-// to see the results, right click on the page and click inspect, and click the console tab
-
+$.get(url_photos, (data) => {
+    let randomPhotos = data.sort(() => Math.random() - 0.5).slice(0,2);
+    console.log(randomPhotos);
 // display the two random photos
+    document.getElementById("photo1").src = randomPhotos[0].thumbnailUrl;
+    document.getElementById("photo2").src = randomPhotos[1].thumbnailUrl;
+})
+// to see the results, right click on the page and click inspect, and click the console tab
