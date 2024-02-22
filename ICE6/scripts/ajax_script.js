b25d918a2ec9af42849c74d67bed351e1b7e67d4
JakobOlive <jakob.olive@dcmail.ca>
2024-02-22 14:07:55 -0500

    Initial commit for ICE6.

---
+++ ICE6/scripts/ajax_script.js
@@ -0,0 +1,18 @@
+console.log('ajax_script.js loaded')
+
+let url_posts = 'https://jsonplaceholder.typicode.com/posts';
+let url_comments = 'https://jsonplaceholder.typicode.com/comments';
+let url_albums = 'https://jsonplaceholder.typicode.com/albums';
+let url_photos = 'https://jsonplaceholder.typicode.com/photos';
+let url_todos = 'https://jsonplaceholder.typicode.com/todos';
+let url_users = 'https://jsonplaceholder.typicode.com/users';
+
+// create the function that uses jquery get
+
+// for each url variable above, call the function in a console.log
+
+// from the photos url, put 2 random photos into the img elements in the html
+
+// to see the results, right click on the page and click inspect, and click the console tab
+
+// display the two random photos
