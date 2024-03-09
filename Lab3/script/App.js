2c2c73b2d6ac13a3f87a8f812f9a84b9a93e546f
JakobOlive <jakob.olive@dcmail.ca>
2024-03-09 15:00:37 -0500

    Blog page is fully built and styled within the app.js and the css file to look somewhat appealing. Next to create the blog.js file and input the data into the posts.

--- Lab3/script/App.js
+++ Lab3/script/App.js
@@ -730,3 +730,46 @@ $(document).ready(function() {
         }
     });
 })
+
+/* SECTION FOR LAB3 */
+// This section checks for the body id for the blogPage, and builds the blog page.
+if (document.getElementById("blogPage")) { 
+    // First an IFFY to create blog cards that will be filled later. This function will iterate 7 times to populate the page with 21 cards,
+    // as the assignment requires us to have at least 20. After the cards are built we will use jquery iteration to grab and sort the results
+    // and populate using data grabbed from AJAX calls.
+    $(function() {
+        for (let i = 0; i <= 6; i++) {
+            let blogCard = `<div class="card-group">
+                                <div class="card">
+                                <img class="card-img-top" src="..." alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                                </div>
+                                <div class="card">
+                                <img class="card-img-top" src="..." alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                                </div>
+                                <div class="card">
+                                <img class="card-img-top" src="..." alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                                </div>
+                            </div>
+                            </br>`;
+            
+        // Appending the cards to the <div> with the content id.
+        
+            $('#content').append(blogCard);
+        }
+    });
+}
---
+++ Lab3/script/Blog.js
@@ -0,0 +1,7 @@
+/*
+    Name: Jakob Olive
+    Date: 2024-03-05
+    Course: INFT 2202
+    File: Blog.js
+    File Desc: 
+*/
