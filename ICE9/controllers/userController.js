22dff57d7fce9a9f6161f4408e0b395994648633
JakobOlive <jakob.olive@dcmail.ca>
2024-04-02 16:04:29 -0400

    Some file organization to get rid of duplicates.

--- ICE9/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Define the homeView method
-module.exports.homeView = function(req, res) {
-    // Render the home template (assuming it's stored in the views directory)
-    res.render('home'); // This will render home.hbs
-};
